896e1275dfb702f47c279fb9c860925f14c057d7
wangzezhong <22895870+wangzezhong@users.noreply.github.com>
2024-04-05 22:09:12 -0700

    Add files via upload

---
+++ assets/js/CardComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card" @click="$emit('zoomIn', post)">
+    <div v-if="post">
+      <div v-if="post.img">
+        <img :src="post.img" alt="Card image" />
+      </div>
+      <div v-else>No image available</div>
+    </div>
+    <div v-else>No post available</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
